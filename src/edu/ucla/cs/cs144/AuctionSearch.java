407f5905b0c0287d100c58334e7e82f6645caf65
cs144 <cs144@cs144.localdomain>
2013-11-08 22:40:10 -0800

    changed helper functions to private

--- src/edu/ucla/cs/cs144/AuctionSearch.java
+++ src/edu/ucla/cs/cs144/AuctionSearch.java
@@ -220,7 +220,7 @@ public class AuctionSearch implements IAuctionSearch {
 		return r;
 	}
 
-	public static String formatXMLDate(String indate) {
+	private static String formatXMLDate(String indate) {
 		Date out = null;
 		SimpleDateFormat outformat = new SimpleDateFormat("MMM-dd-yy HH:mm:ss");
 		SimpleDateFormat informat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -233,7 +233,7 @@ public class AuctionSearch implements IAuctionSearch {
 		return outformat.format(out);
 	}
 	
-	public static String escapeXML( String s ) {
+	private static String escapeXML( String s ) {
 		s = s.replace("&", "&amp;");
 		s = s.replace("\"", "&quot;");
 		s = s.replace("\'", "&apos;");
